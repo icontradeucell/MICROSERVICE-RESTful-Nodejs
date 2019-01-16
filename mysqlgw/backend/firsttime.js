0086bff8fa3723922d3fb2acc35506ebaf3b0c74
tudtude <tudtude@gmail.com>
2019-01-16 10:34:30 +0700

    update mysqlgw

--- mysqlgw/backend/firsttime.js
+++ mysqlgw/backend/firsttime.js
@@ -1,29 +1,29 @@
 
 module.exports = ( User, conn, log, bcrypt ) => {
 
-    conn.sync()
-    User.findOne({
-        where: {
-            username: 'admin'
-        }
-    }).then( res=> {
-        if( res ) {
-            log( bcrypt.compareSync( 'admin', res.pwd )) 
-            log("Account admin already set")
-            return null
-        }
-        User.create({
-            username : 'admin',
-            email : 'admin@system.com',
-            method : 'local',
-            pwd : 'admin',
-            role: 'admin'
-        }).then( res => {
-            log("Account admin was create")
-        }).catch( err => {
-            log("There is err", err )
+    conn.sync().then( () => {
+
+        User.findOne({
+            where: {
+                username: 'admin'
+            }
+        }).then( res=> {
+            if( res ) {
+                log( bcrypt.compareSync( 'admin', res.pwd )) 
+                log("Account admin already set")
+                return null
+            }
+            User.create({
+                username : 'admin',
+                email : 'admin@system.com',
+                method : 'local',
+                pwd : 'admin',
+                role: 'admin'
+            }).then( res => {
+                log("Account admin was create")
+            }).catch( err => {
+                log("There is err", err )
+            })
         })
     })
-
-    return User
 }
